b95004842fa9aaedf3050a5bfec8362a83b92733
Dmytro-Ivanenko <dmitro0399@gmail.com>
2023-06-03 01:07:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/test_task_g">
             <App />
         </BrowserRouter>
     </React.StrictMode>
